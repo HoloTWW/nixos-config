3b412d6fa97fbf56017345f745283a52ef432d7f
HoloTWW <ivanchenkov.business@yandex.ru>
2026-03-02 20:05:23 +0300

    [bluetooth] fix connections

--- nixos/modules/sound.nix
+++ nixos/modules/sound.nix
@@ -1,18 +1,38 @@
-{
-  hardware.pulseaudio.enable = false;
-  #sound.enable = true;
+{pkgs,...}:{
+  
+  environment.systemPackages = with pkgs; [
+    # gui mixer   
+    pavucontrol
+  ];
 
-  # rtkit is optional but recommended
+  # disble to avoid conflicts
+  hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
 
   services.pipewire = {
     enable = true;
-
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    
+    # session manager
+    wireplumber.enable = true;
 
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    # someday i will go back to the reaper-daw...someday...
+    jack.enable = true;
+  };
+
+  services.pipewire.wireplumber.extraConfig = {
+    "monitor.bluez.properties" = {
+      "bluez5.enable-sbc-xq" = true;
+      "bluez5.enable-msbc" = true;
+      "bluez5.enable-hw-volume" = true;
+      "bluez5.roles" = [ "a2dp_sink" "a2dp_source" "headset_head_unit" "headset_audio_gateway" ];
+    };
+    "10-bluetooth-policy" = {
+      "monitor.bluez.properties" = {
+        "bluez5.enable-hw-volume" = true; # sync system volume (idk will it works)
+      };
+    };
   };
 }
